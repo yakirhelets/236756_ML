fd8d1fc62c3bc0e38cdc96fc5a85f830a27611db
Gal Fleissig <fleissigal@gmail.com>
2019-04-24 17:57:00 +0300

    Add code for the sections and organize code

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -2,14 +2,23 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
-# Mandatory assignment:
+# -------------------------------------------------------------------
+# ------------------------ 1: Loading the data ----------------------
+# -------------------------------------------------------------------
+
 # Load the Election Challenge data from the ElectionsData.csv
-file_name = 'ElectionsData.csv'
-data_frame = pd.read_csv(file_name)
+elections_file = 'ElectionsData.csv'
+elections_data = pd.read_csv(elections_file)
+# print(elections_data)
+
+# -------------------------------------------------------------------
+# ------------------------ 2: Identify and set the type -------------
+# -------------------------------------------------------------------
 
 # Identify and set the correct type of each attribute
-
 
 
 # lst = list(data_frame._get_axis(1))
@@ -17,15 +26,25 @@ data_frame = pd.read_csv(file_name)
 # data_frame.astype()
 # print(data_frame.sample(1).dtypes)
 
+# TODO: make sure the datatypes are also int etc.
+# TODO: probably after imputation, if not - ask
+# print(elections_data.iloc[[1]].dtypes)
+
+
+
+# -------------------------------------------------------------------
+# ------------------------ 3: Split the data ------------------------
+# -------------------------------------------------------------------
+
 # Split the data to train, test, validation sets
-labels = data_frame.iloc[:, 0]  # labels
-data = data_frame.iloc[:, 1:]  # data
+data = elections_data.iloc[:, 1:]  # data
+labels = elections_data.iloc[:, 0]  # labels
 X_train, X_test, Y_train, Y_test =\
     train_test_split(data, labels, train_size=0.9, test_size=0.1, shuffle=False,
-                     random_state=None, stratify=None) #todo: changes train size and shuffle
+                     random_state=None, stratify=None) #TODO: changes train size and shuffle
 X_train, X_validation, Y_train, Y_validation =\
     train_test_split(X_train, Y_train, train_size=0.83, test_size=0.17, shuffle=False,
-                     random_state=None ,stratify=None) #todo: changes train size and shuffle
+                     random_state=None ,stratify=None) #TODO: changes train size and shuffle
 
 X_train_prep = X_train.copy()
 X_validation_prep = X_validation.copy()
@@ -35,16 +54,114 @@ X_test_prep = X_test.copy()
 Y_test_prep = Y_test.copy()
 
 # Data preparation actions on the training set:
-# Imputation
+
+# -------------------------------------------------------------------
+# ------------------------ 4: Data preparation actions --------------
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# ------------------------ A: Imputation ----------------------------
+# -------------------------------------------------------------------
+
+# Method 1: Remove examples with NaN
+
+X_train_dropped_NaN = X_train_prep.dropna()
+print(X_train_dropped_NaN.shape)
+
+# Method 2: Single Value Assignment - Mode
+#TODO: for all columns
 mod = stats.mode(X_train_prep["Occupation_Satisfaction"])
-print(mod[0][0])
-xx = X_train_prep["Occupation_Satisfaction"].fillna(mod[0][0])
-print(xx)
+X_train_prep["Occupation_Satisfaction"].fillna(mod[0][0])
+# print(X_train_prep)
 
-# Data cleansing
-# Normalization
-# Feature selection
+# Method 3:
 
 
+# -------------------------------------------------------------------
+# ------------------------ B: Data Cleansing ------------------------
+# -------------------------------------------------------------------
+
+# ------------------------ B1: Type/Value Modification --------------
+
+# Binominal (converting to [-1,1])
+
+# TODO: fill in
+
+# Multinominal/Polynominal
+
+# TODO: fill in
+
+
+# print(X_train_prep.values[:, 2])
+
+# ------------------------ B2: Outlier Detection --------------------
+
+# Printing the correlation matrix TODO: research correlation in pandas more
+# TODO: right now shows only the 27 numerical features, need to convert caterogical to numerical before running
+plt.matshow(X_train_prep.corr())
+plt.show()
+# histogram_intersection = lambda a, b: np.minimum(a, b).sum().round(decimals=1)
+# X_train_prep.corr(method=histogram_intersection)
+
+# outlier detection - 3 examples of correlated features #TODO: provide more examples based on the correlation matrix
+plt.scatter(X_train_prep.Avg_monthly_expense_when_under_age_21, X_train_prep.Avg_monthly_expense_on_pets_or_plants)
+plt.show()
+
+# TODO: write code that eliminates examples beyond a certain range, for each attribute
+
+# outliers_estimator = Estimator()
+# outliers_detection = estimator.fit(X_train_prep)
+# print(outliers_detection)
+
+
+# -------------------------------------------------------------------
+# ------------------------ C: Normalization -------------------------
+# -------------------------------------------------------------------
+
+# TODO: fill in
+
+# First we check the distribution of each column to see whether to apply
+# Z-score on it or Min-Max:
+
+# TODO: plot the histogram for each column of X_train_prep and check if it's normally or uniformly distributed
+
+
+# Z-score:
+
+column_z_score = X_train_prep["Avg_monthly_expense_when_under_age_21"]
+X_train_prep["Avg_monthly_expense_when_under_age_21"] = stats.zscore(column_z_score)
+
+# Min-Max:
+
+# TODO: determine later for each attribute that we are going
+# TODO: to apply min-max on if [-1,1] is right (leave as is) or we need a different range
+# TODO: then create a new scaler with the required range
+
+# scaler = MinMaxScaler(feature_range=(-1,1))
+
+# column_min_max = X_train_prep["Avg_monthly_expense_when_under_age_21"]
+# X_train_prep["Avg_monthly_expense_when_under_age_21"] = scaler.transform(column_min_max)
+
+
+
+# -------------------------------------------------------------------
+# ------------------------ D: Feature Selection ---------------------
+# -------------------------------------------------------------------
+
+# TODO: fill in
+
+
+# -------------------------------------------------------------------
+# ------------------------ 5: Apply changes on other sets -----------
+# -------------------------------------------------------------------
+
+# TODO: fill in
+
+
+# -------------------------------------------------------------------
+# ------------------------ 6: Saving the prepared data --------------
+# -------------------------------------------------------------------
 
 # Save the 3x2 data sets in CSV files
+
+# TODO: fill in
